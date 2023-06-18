ad88d3e0ee01067ded3ca786492df6044b9f06bf
ryan2414 <jackpar@nate.com>
2023-06-18 23:47:15 +0900

    Study StoryBoard With Cord

---
+++ NextVC-시작/NextVC/FirstViewController.swift
@@ -0,0 +1,65 @@
+//
+//  FirstViewController.swift
+//  NextVC
+//
+//  Created by 이승재 on 2023/06/17.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    let mainLabel = UILabel()
+    let backButton: UIButton = {
+        let button = UIButton(type: .custom)
+        
+        button.setTitle("Back", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .blue
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        button.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    var someString: String?
+    
+     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setup()
+        makeAutoLayout()
+    }
+    
+    func setup(){
+        mainLabel.text = someString
+        mainLabel.font = UIFont.systemFont(ofSize: 22)
+        
+        view.addSubview(mainLabel)
+        view.addSubview(backButton)
+        
+        view.backgroundColor = .gray
+    }
+    
+    func makeAutoLayout() {
+        mainLabel.translatesAutoresizingMaskIntoConstraints = false
+        mainLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        mainLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+        backButton.translatesAutoresizingMaskIntoConstraints = false
+        backButton.widthAnchor.constraint(equalToConstant: 70).isActive = true
+        backButton.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        backButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -40).isActive = true
+        backButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    }
+    
+    @objc func backButtonTapped() {
+        dismiss(animated: true)//이전화면으로 돌아가기
+    }
+    
+
+    
+    
+    
+}
