f8a47d4cbc60a1ffcf3cdb62f88b04cab00dc0f9
ryan2414 <jackpar@nate.com>
2023-06-21 21:26:54 +0900

    Table View 만들기

---
+++ TableView/TableView/Models/Movie.swift
@@ -0,0 +1,15 @@
+//
+//  Movie.swift
+//  TableView
+//
+//  Created by 이승재 on 2023/06/21.
+//
+
+import UIKit
+
+struct Movie {
+    var movieImage: UIImage?
+    let movieName: String
+    let movieDescription: String
+     
+}
---
+++ TableView/TableView/Veiws/MovieCell.swift
@@ -0,0 +1,27 @@
+//
+//  MovieCell.swift
+//  TableView
+//
+//  Created by 이승재 on 2023/06/21.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var mainImageView: UIImageView!
+    @IBOutlet weak var movieNameLabel: UILabel!
+    @IBOutlet weak var movieDescription: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
